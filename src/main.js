dea30481c300c06537c38409a3e88736cf8d61a7
Aliorpse <Aliorpse@qq.com>
2025-03-08 01:15:58 +0800

    dep

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
